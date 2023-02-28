6c237dd9d108ea160d64c99339b0d36f05075eee
Shousuke Komi <korea.ashik@gmail.com>
2023-03-01 01:40:37 +0600

    Snow

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+         nix = lib.nixosSystem {
+          inherit system pkgs;
+          modules = [ 
+            ./configuration.nix
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.nix = {
+                imports = [ ./home.nix ];
+              };
+            }
+          ];
+        };
+      };
+    };
+}
+
---
+++ home.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "nix";
+  home.homeDirectory = "/home/nix";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+    home.packages = with pkgs; [
+         btop
+         neovim
+         geany
+	 mpv
+	 rofi
+	 wofi
+	 git
+  ];
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
