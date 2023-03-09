629197a616be2368072d5898958c6b8713fb01fd
Shousuke Komi <109201315+Komi7@users.noreply.github.com>
2023-03-09 19:18:28 +0600

    add Nvidia driver.

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,8 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./fonts.nix
+      ./bspwm.nix
     ];
 
   # Bootloader.
@@ -45,12 +47,12 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-#  services.xserver.videoDrivers = [ "nvidiaLegacy470" ];
   services.xserver.videoDrivers = [ "nvidia"];
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
   hardware.opengl.enable = true;
+
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
@@ -58,41 +60,26 @@
     layout = "us";
     xkbVariant = "";
   };
-  
-#  {
-  # NVIDIA drivers are unfree.
-#   nixpkgs.config.allowUnfree = true;
-
-#  services.xserver.videoDrivers = [ "nvidiaLegacy470" ];
-#  hardware.opengl.enable = true;
-
-  # Optionally, you may need to select the appropriate driver version for your specific GPU.
- # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-  # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
- #  hardware.nvidia.modesetting.enable = true;
-#  ...
-#  }; 
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+  hardware.pulseaudio.enable = true;
+ # security.rtkit.enable = true;
+ # services.pipewire = {
+ #   enable = true;
+ #   alsa.enable = true;
+ #   alsa.support32Bit = true;
+ #   pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-  };
+ # };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -115,8 +102,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    wget
+    curl
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -130,7 +117,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+   services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -145,5 +132,17 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
+nix = {
+  package = pkgs.nixFlakes;
+  extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+};
+  # Fonts conf
+  # fonts.fonts = with pkgs; [
+ # (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "Hack" ]; })
+# ];
 
+#auto upgrade nixos
+system.autoUpgrade.enable = true;
 }
