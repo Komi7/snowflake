eb17fa8b3372bc7907786a8cf42c9df87f099b5a
komi7 <korea.ashik@gmail.com>
2026-03-06 12:08:23 +0600

    waybar style1

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -49,6 +49,30 @@
           max-length = 35;
         };
 
+        # Full Interactive Calendar
+        "clock" = {
+          format = "{:%I:%M %p}";
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+          calendar = {
+            mode          = "year";
+            mode-mon-col  = 3;
+            weeks-pos     = "right";
+            on-scroll     = 1;
+            format = {
+              months =   "<span color='#ffead3'><b>{}</b></span>";
+              days =     "<span color='#ecc6d9'><b>{}</b></span>";
+              weeks =    "<span color='#99ffdd'><b>W{}</b></span>";
+              weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+              today =    "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+          };
+          actions = {
+            on-click-right = "mode";
+            on-scroll-up = "shift_up";
+            on-scroll-down = "shift_down";
+          };
+        };
+
         # Hardware Monitoring Group
         "group/hardware" = {
           orientation = "horizontal";
@@ -63,11 +87,6 @@
         "memory" = { 
           format = " {percentage}%";
           interval = 10;
-        };
-
-        "clock" = {
-          format = "{:%I:%M %p}";
-          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
         };
 
         "pulseaudio" = {
